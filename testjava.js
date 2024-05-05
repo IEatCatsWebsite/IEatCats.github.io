4dd03204bd7ed742081752f1e2336f1ff5f48c49
IEatCatsWebsite <168598801+IEatCatsWebsite@users.noreply.github.com>
2024-05-05 14:39:42 -0400

    Update testjava.js

--- testjava.js
+++ testjava.js
@@ -4,3 +4,4 @@ let test = "test";
 document.getElementById("var1test").innerHTML = test;
 let question = window.prompt("Is this real?");
 console.log(question);
+document.getElementById("var2test").innerHTML = question;
