31564c324e4b713b68fc35e445690c679530de9a
IEatCatsWebsite <168598801+IEatCatsWebsite@users.noreply.github.com>
2024-05-06 12:27:40 -0400

    Update testjava.js

--- testjava.js
+++ testjava.js
@@ -5,3 +5,8 @@ document.getElementById("var1test").innerHTML = test;
 let question = window.prompt("Is this real?");
 console.log(question);
 document.getElementById("var2test").innerHTML = question;
+
+document.getElementById("buttontest").innerHTML = button(){
+
+  document.getElementById("inputtest").innerHTML = headertest;
+}
